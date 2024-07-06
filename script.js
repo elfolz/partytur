7560fea7f64d95a159f04fd9147f88053cfd8679
Luiz <lhvalentim@outlook.com>
2024-07-06 13:04:02 -0300

    progress

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var table, canvas, ctx, cw, ch
 
 const notes = []
 const margin = [20, 60]
-const startPosition = 40
+const startPosition = 60
 const spacing = 25
 const lineHeight = 12
 const rhythms = {
@@ -25,10 +25,11 @@ const pixelRatio = () => {
   return dpr / bsr
 }
 
+let pieceClef = 'g4'
+let pieceTempo = [4, 4]
 let currentPosition = 0
 let currentRhythm = 3
 let isBreak = false
-let pieceClef = 'g4'
 
 function init() {
 	table = document.querySelector('table')
@@ -40,10 +41,8 @@ function init() {
 }
 
 function resize() {
-	cw = canvas.width = (window.innerWidth * pixelRatio())
-	ch = canvas.height = (window.innerHeight * pixelRatio())
-	ctx.font = "42px Noto Music"
-	ctx.fillStyle = 'whitesmoke'
+	cw = canvas.width = window.innerWidth
+	ch = canvas.height = window.innerHeight
 	ctx.setTransform(pixelRatio(), 0, 0, pixelRatio(), 0, 0)
 }
 
@@ -52,15 +51,16 @@ function refresh() {
 	requestAnimationFrame(refresh)
 	ctx.clearRect(0, 0, cw, ch)
 	drawPentagram(margin[0], margin[1])
-	drawCarriage(margin[0], margin[1])
 	drawClef(margin[0], margin[1])
+	drawTempo(margin[0], margin[1])
+	drawCarriage(margin[0], margin[1])
 	drawNotes()
 	refreshTable()
 }
 
 function drawPentagram(x, y) {
 	ctx.beginPath()
-	ctx.globalAlpha = 0.25
+	ctx.fillStyle = '#646464'
 	for (let i=0; i<5; i++) {
 		ctx.fillRect(x, y+(i*lineHeight), cw-(x*2), 1)
 	}
@@ -68,17 +68,38 @@ function drawPentagram(x, y) {
 	ctx.fillRect(cw-x, y, 1, 4*lineHeight+1)
 }
 
+function drawHelperLines(x, y) {
+	const h = margin[1] + (lineHeight*4)
+	let quant = Math.abs(y - h) / lineHeight
+	if (h > y) quant -= 3
+	ctx.beginPath()
+	ctx.fillStyle = '#646464'
+	for (let i=0; i<quant; i++) {
+		const posy = y > h ? margin[1]+(lineHeight*(4+i)) : margin[1]-(lineHeight*i)
+		ctx.fillRect(x-4, posy, spacing, 1)
+	}
+}
+
 function drawClef(x, y) {
 	ctx.beginPath()
-	ctx.globalAlpha = 1
+	ctx.fillStyle = 'whitesmoke'
+	ctx.font = "42px Noto Music"
 	ctx.fillText("𝄞", x, y+46)
+}
+
+function drawTempo(x, y) {
+	const posx = x + 35
+	ctx.beginPath()
+	ctx.fillStyle = 'whitesmoke'
+	ctx.font = "22px Noto Music"
+	ctx.fillText(pieceTempo[0], posx, y+20)
+	ctx.fillRect(posx, y+24, 15, 1)
+	ctx.fillText(pieceTempo[1], posx, y+44)
 }
 
 function drawNotes() {
 	notes.forEach((el, i) => {
 		let pos = 0
-		ctx.beginPath()
-		ctx.globalAlpha = 1
 		if (el[0] == 'a') pos = initialNotePosition()
 		else if (el[0] == 'b') pos = initialNotePosition() - (lineHeight/2)
 		else if (el[0] == 'c') pos = initialNotePosition() - ((lineHeight/2)*2)
@@ -86,20 +107,26 @@ function drawNotes() {
 		else if (el[0] == 'e') pos = initialNotePosition() - ((lineHeight/2)*4)
 		else if (el[0] == 'f') pos = initialNotePosition() - ((lineHeight/2)*5)
 		else if (el[0] == 'g') pos = initialNotePosition() - ((lineHeight/2)*6)
-		ctx.fillText(rhythms[el[1]][el[2] ? 1 : 0], margin[0] + startPosition + (i*spacing), margin[1]+pos+(lineHeight*el[3]))
+		const x = margin[0] + startPosition + (i*spacing)
+		const y = margin[1] + pos + ((lineHeight/2)*el[3])
+		drawHelperLines(x, y)
+		ctx.beginPath()
+		ctx.fillStyle = 'whitesmoke'
+		ctx.font = "42px Noto Music"
+		ctx.fillText(rhythms[el[1]][el[2] ? 1 : 0], x, y)
 	})
 }
 
 function drawCarriage(x, y) {
 	const px = x + startPosition + (notes.length*spacing)
 	ctx.beginPath()
-	ctx.globalAlpha = performance.now() % 500 < 250 ? 1 : 0
+	ctx.fillStyle = performance.now() % 500 < 250 ? 'whitesmoke' : 'transparent'
 	ctx.fillRect(px, y-6, 1, 5*lineHeight)
 }
 
 function refreshTable() {
 	let px = margin[0] + (notes.length*spacing) - (table.clientWidth / 2)
-	let py = (margin[1] + (lineHeight*4) + 30) * pixelRatio()
+	let py = (margin[1] + (lineHeight*4) + 30) * pixelRatio() + 10
 	if (px < 0) px = 0
 	if (px > cw - table.clientWidth) px = cw - table.clientWidth
 	table.style.setProperty('left', `${px}px`)
@@ -152,8 +179,8 @@ window.onkeydown = e => {
 	}
 	if (['a', 'b', 'c', 'd', 'e', 'f', 'g'].includes(key)) {
 		let octave = 0
-		if (e.ctrlKey) octave = 1
-		if (e.shiftKey) octave = -1
+		if (e.ctrlKey) octave = 7
+		if (e.shiftKey) octave = -7
 		notes.push([key, currentRhythm, isBreak, octave])
 		currentPosition++
 	}
